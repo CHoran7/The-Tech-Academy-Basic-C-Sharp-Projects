314e52112b5474e021c489e142e6454e58694ab9
Cole Horan <colehoran@gmail.com>
2019-10-11 21:00:54 -0600

    Step 106
    Here is some try/catch code for step 106 of the C# course.

---
+++ step106/step106/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step106
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numbers = new List<int>() { 1,4,9,16,25,36 };
+                Console.WriteLine("Please select a number.");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+            
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.WriteLine(numbers[i] / number1);
+                }
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
