11af695a6e612357061e877ceff5beb798c454ee
Cole Horan <colehoran@gmail.com>
2019-10-14 19:38:08 -0600

    Step 114
    Completed step 114 of the C# course.

---
+++ step114/step114/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step114
+{
+    class Class1
+    {
+        public static int Method1(int integer)
+        {
+            int bigger = integer * 100;
+            return bigger;
+        }
+
+        public static int Method1(decimal dec)
+        {
+            int rounded = Convert.ToInt32(dec);
+            return rounded;
+        }
+
+        public static int Method1(string number)
+        {
+            int calc = Convert.ToInt32(number);
+            calc = calc + 5;
+            return calc;
+        }
+    }
+}
---
+++ step114/step114/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = Class1.Method1(12);
+            Console.WriteLine(value);
+            decimal pi = 3.14159m;
+            int rounded = Class1.Method1(pi);
+            Console.WriteLine(rounded);
+            int fiveBigger = Class1.Method1("4");
+            Console.WriteLine(fiveBigger);
+            Console.ReadLine();
+        }
+    }
+}
