a7669afad8edecbf24876aceae90178f546e9e38
Cole Horan <colehoran@gmail.com>
2019-10-14 20:01:06 -0600

    Step 115
    Completed step 115 of the C# course.

---
+++ step115/step115/Power.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step115
+{
+    class Power
+    {
+        public static int Exponent(int mathBase, int exponent = 2)
+        {
+            double temp1 = Convert.ToDouble(mathBase);
+            double temp2 = Convert.ToDouble(exponent);
+
+            int result = Convert.ToInt32(Math.Pow(temp1, temp2));
+            return result;
+        }
+    }
+}
---
+++ step115/step115/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide a number.");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Would you like to provide a second number? y/n");
+            string check = Console.ReadLine();
+            if(check == "y")
+            {
+                Console.WriteLine("What number?");
+                int userNum2 = Convert.ToInt32(Console.ReadLine());
+                int result = Power.Exponent(userNum1, userNum2);
+                Console.WriteLine("{0} to the power of {1} is {2}", userNum1, userNum2, result);
+            }
+            else
+            {
+                int result = Power.Exponent(userNum1);
+                Console.WriteLine("{0} to the power of 2 is {1}", userNum1, result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
