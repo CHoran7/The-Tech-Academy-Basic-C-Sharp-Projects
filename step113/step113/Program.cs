b368f8fb6450d370efd1086831e93a2342c0e1fd
Cole Horan <colehoran@gmail.com>
2019-10-14 19:16:30 -0600

    Step 113
    Completed step 113 of the C# course.

---
+++ step113/step113/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Void.Out(3, 5);
+            Void.Out(one: 2, two: 5);
+        }
+    }
+}
---
+++ step113/step113/Void.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step113
+{
+    class Void
+    {
+        public static void Out(int one, int two)
+        {
+            int half = one / 2;
+            Console.WriteLine(two);
+            Console.ReadLine();
+        }
+    }
+}
