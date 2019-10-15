56fd92901f8a8b6bf46f68fdf130922d9a44616a
Cole Horan <colehoran@gmail.com>
2019-10-14 18:47:45 -0600

    step 111
    Completed step 11 of the C# course.

---
+++ step111/step111/MathMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step111
+{
+    class MathMethods
+    {
+        public static int Square(int integer)
+        {
+            int square = integer * integer;
+            return square;
+        }
+
+        public static int Doubling(int integer)
+        {
+            int dub = 2 * integer;
+            return dub;
+        }
+
+        public static int Triple(int integer)
+        {
+            int triple = 3 * integer;
+            return triple;
+        }
+    }
+}
---
+++ step111/step111/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like to do operations on?");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            int dub = MathMethods.Doubling(userNum);
+            Console.WriteLine("Double your number is: " + dub);
+            int trip = MathMethods.Triple(userNum);
+            Console.WriteLine("Triple your number is: " + trip);
+            int square = MathMethods.Square(userNum);
+            Console.WriteLine("The square of your number is: " + square);
+            Console.ReadLine();
+        }
+    }
+}
