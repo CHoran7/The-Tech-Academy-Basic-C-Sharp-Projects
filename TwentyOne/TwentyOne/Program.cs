354ac1e50b263379b250e8d66e2ca2b468e8fcf6
Cole Horan <colehoran@gmail.com>
2019-10-18 22:06:42 -0600

    Step 120
    Complete step 120 of the C# course dealing with class inheritance.

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,15 +10,20 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck();
-            deck.Shuffle();
-            
-            foreach(Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Cole", "Tucker", "CJ" };
+            game.ListPlayers();
+            game.Play();
             Console.ReadLine();
+            //Deck deck = new Deck();
+            //deck.Shuffle();
+            
+            //foreach(Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
+            //Console.ReadLine();
         }
 
         
