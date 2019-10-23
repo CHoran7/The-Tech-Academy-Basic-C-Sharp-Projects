f4f4cd21cf87d98d0872f50311e58c8871cffa1a
Cole Horan <colehoran@gmail.com>
2019-10-22 18:46:21 -0600

    Step 137
    Completed step 137 of the C# course implementing a struct.

---
+++ step137/step137/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 3.14159m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
