2d092638650a535edb58a01ac47f6da7cd0864d0
Cole Horan <colehoran@gmail.com>
2019-10-30 22:47:04 -0600

    Step 154 and 21 game
    Completed step 154 of the C# course using logs. Also followed along and have a working project of the twenty one game.

---
+++ step154/step154/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace step154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide a number: ");
+            string userNum = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Cole\Documents\log2.txt", userNum);
+            string text = File.ReadAllText(@"C:\Users\Cole\Documents\log2.txt");
+            Console.WriteLine(text);
+            Console.Read();
+        }
+    }
+}
