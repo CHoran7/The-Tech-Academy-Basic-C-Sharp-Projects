a87ef4d0eb968abb3e7cd52eaa4ff18c661f35d9
Cole Horan <colehoran@gmail.com>
2019-11-02 10:39:28 -0600

    step 156
    Worked with some code with the DateTime objects.

---
+++ step 156/step 156/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please provide a number: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            DateTime future = new DateTime();
+            future = DateTime.Now.AddHours(userNum);
+            Console.WriteLine("In {0} hours it will be {1}.", userNum, future);
+            Console.Read();
+        }
+    }
+}
